1be008591694da60e5d592add14aacf39fa8394f
DK\psk <psk@sitecore.net>
2017-08-28 16:11:36 -0400

    Fix issue #181026

---
+++ src/Sitecore.Support.181026/Jobs/JobRunner.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.Support.Jobs
+{
+    using Sitecore.Jobs;
+    using Sitecore.Configuration;
+    public class JobRunner
+    {
+        public static void SetPriority(JobArgs args)
+        {
+            if (args.Job.Options.Method.Method.Name == "PublishWithResult" && args.Job.Options.Category == "publish" && args.Job.Options.SiteName == "publisher")
+            {
+                args.Job.Options.Priority = Settings.Publishing.ThreadPriority;
+            }
+            Sitecore.Jobs.JobRunner.SetPriority(args);
+        }
+    }
+}
---
+++ src/Sitecore.Support.181026/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.181026")]
+[assembly: AssemblyProduct("Sitecore.Support.181026")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("The Publishing.ThreadPriority setting does not change thread's priority")]
+
